9f9045f01e706e448389ad9e5c0f374fabd2bd86
khanAston <122929455+khanAston@users.noreply.github.com>
2023-11-13 17:31:52 +0300

    Update Lab01_Java.java
    Template file created.

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -1,2 +1,23 @@
 //Lab01 Java file
 // Sample Java file
+void testMethod1()
+  {
+  //First command x = y + z
+  //Second commnad print(x)
+
+
+  //Last line print('Good bye')
+}
+
+void testMethod2()
+  {
+  // First command x = //to be filled in by student
+  // Second command 
+  // Third command
+}
+
+void main()
+  {
+    testMethod1();
+    testMethod2();
+}
