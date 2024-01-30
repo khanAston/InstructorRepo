2d23767b273b66e2da98c2c0508b3bbdaea95bff
HBagash <70002281+HBagash@users.noreply.github.com>
2024-01-30 16:44:11 +0000

    Update Lab01_Java.java
    Changes made by student

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,9 +4,9 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
+  //Edit made by Student-X
 
-
-  //Last line print('Good bye')
+  //Last line print('TA TA')
 }
 
 void testMethod2()
