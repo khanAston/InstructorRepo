f559e8c08bc05cefbc71fd2003f895bc8d929f3c
Wise Old Duck <ABIMUSTA042@gmail.com>
2025-01-30 14:39:31 +0000

    Edited Lab 2 by the big tyme geeky member

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-  // calm luh edit made by Wise Old Duck in GitPod
+  // calm g edit made by Wise Old Duck in GitPod
 
 
   //Last line print('TURNMEUP')
